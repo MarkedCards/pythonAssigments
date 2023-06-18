2be1c2c62281192e47fcffddb7d42e7e405c1556
MarkedCards <dolphinkick2003@gmail.com>
2023-06-17 22:21:50 -0700

    file

---
+++ CIS129_BryanLoflin_CookieLab.py
@@ -0,0 +1,7 @@
+#Bryan Loflin
+#converts input into intger
+cookiesEaten = int( input('How many cookies have you eaten:'))
+#total calories eaten
+calories = cookiesEaten * 75
+#prints result
+print(calories)
